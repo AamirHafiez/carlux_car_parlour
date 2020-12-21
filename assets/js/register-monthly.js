ff2b794ee05896ef8e5490fdf8f82298af7d41ab
Aamir <aamirhafiez@gmail.com>
2020-12-21 21:58:50 +0530

    removed login, registration and demo pages | added tap to know more for better mobile UI

--- assets/js/register-monthly.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('#contact-copy').click(() => {
-        console.log('clicked');
-        let x = $('#contact-number').val();
-        $('#whatsapp-contact').val(x);
-    });
-});
